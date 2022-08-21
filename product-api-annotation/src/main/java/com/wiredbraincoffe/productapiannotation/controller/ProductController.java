5e1d80e1e009cd7f29bdaa8087b518e42c714aff
Daniel <danielbou@outlook.com.br>
2022-08-21 15:52:57 -0300

    Server-side Events

--- product-api-annotation/src/main/java/com/wiredbraincoffe/productapiannotation/controller/ProductController.java
+++ product-api-annotation/src/main/java/com/wiredbraincoffe/productapiannotation/controller/ProductController.java
@@ -1,13 +1,17 @@
 package com.wiredbraincoffe.productapiannotation.controller;
 
 import com.wiredbraincoffe.productapiannotation.model.Product;
+import com.wiredbraincoffe.productapiannotation.model.ProductEvent;
 import com.wiredbraincoffe.productapiannotation.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.time.Duration;
 
 @RestController
 @RequestMapping("/products")
@@ -59,4 +63,11 @@ public class ProductController {
     public Mono<Void> deleteAllProducts() {
         return repository.deleteAll();
     }
+
+    @GetMapping(value = "/events", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<ProductEvent> getProductEvents() {
+        return Flux.interval(Duration.ofSeconds(1))
+                .map(val -> new ProductEvent(val,"Product Event"));
+    }
+
 }
